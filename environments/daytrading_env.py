806b879aaeeb33ce9e25f569f13bdb4c2a304c54
Wendell Júnior <wjoj@cin.ufpe.br>
2021-08-24 19:21:53 -0300

    Add total reward, portfolio value, action and portfolio value history to the environment; New shape for action and observation spaces

--- environments/daytrading_env.py
+++ environments/daytrading_env.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from typing import Union
+from warnings import warn
 
 import gym
 import numpy as np
@@ -9,20 +10,24 @@ import pandas as pd
 class DayTradingEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, df, stock_symbols, date_range, W=2, commission_rate=0.00023660):
+    def __init__(self, df, stock_symbols, date_range, window_length=2, commission_rate=0.00023660):
         self.df = self.__preprocess_df(df, stock_symbols, date_range)
         self.symbols = stock_symbols
 
-        self.W = W
+        self.W = window_length
         self.N = len(self.symbols) + 1
-        self.beta = 1 - commission_rate
+        self.beta = 1.0 - commission_rate
 
-        self.__windows = []
-        self.step_count = 0
-        self.state = None
+        self.step_count = None
+        self.observations = None
+        self.observation = None
+        self.total_reward = None
+        self.portfolio_value = None
+        self.portfolio_value_hist = None
+        self.action_hist = None
 
-        self.action_space = gym.spaces.Box(low=0, high=1.0, shape=(self.N,), dtype=np.float32)
-        self.observation_space = gym.spaces.Box(low=0.0, high=10.0, shape=(self.W, self.N), dtype=np.float32)
+        self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(self.N,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=-100.0, high=100.0, shape=(self.W, self.N), dtype=np.float32)
 
         self.reset()
 
@@ -30,38 +35,65 @@ class DayTradingEnv(gym.Env):
         if not self.action_space.contains(action):
             raise ValueError("Action {} does not belong to the action space {}".format(action, self.action_space))
 
-        action = np.asarray(action)
-        action /= sum(action)
-
         self.step_count += 1
-        self.state = self.__get_observation()
+        self.observation = self.__get_observation()
+        reward = self.__calc_reward(self.__normalize_action(action))
+        done = (self.step_count + 1) >= len(self.observations)
+        info = {
+            'total_reward': self.total_reward,
+            'portfolio_value': self.portfolio_value
+        }
 
-        reward = self.__calc_reward(action)
-        done = (self.step_count + 1) >= len(self.__windows)
-
-        return self.state, reward, done, {}
+        return self.__normalize_observation(self.observation), reward, done, info
 
     def reset(self):
-        self.__windows = [w for w in self.df.rolling(self.W)]
         self.step_count = 0
-        self.state = self.__get_observation()
+        self.observations = [w for w in self.df.rolling(self.W)]
+        self.observation = self.__get_observation()
+        self.total_reward = 0
+        self.portfolio_value = 1
+        self.portfolio_value_hist = pd.Series([1.0], index=[self.df.index[0]], dtype=np.float32)
+        self.action_hist = pd.DataFrame([[1.0, *np.zeros(self.N - 1)]], index=[self.df.index[0]],
+                                        columns=['$', *self.df.columns], dtype=np.float32)
 
-        return self.state
+        return self.__normalize_observation(self.observation)
 
     def render(self, mode='human'):
         with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 1000):
-            print(self.__windows[self.step_count])
+            print(self.observations[self.step_count])
 
     def __get_observation(self):
-        window = self.__windows[self.step_count]
+        window = self.observations[self.step_count]
 
-        return np.insert(
-            np.concatenate((np.ones((self.W - len(window), self.N - 1)), np.asarray(window)), axis=0),
-            0, 1.0, axis=1
-        )
+        return np.insert(np.concatenate((np.ones((self.W - len(window), self.N - 1)), window), axis=0), 0, 1.0, axis=1)
 
     def __calc_reward(self, action):
-        return np.log(self.beta * np.dot(self.state[-1], action))
+        profit = self.beta * np.dot(self.observation[-1], action)
+        reward = np.log(profit)
+
+        self.total_reward += reward
+        self.portfolio_value *= profit
+        self.portfolio_value_hist.at[self.df.index[self.step_count]] = self.portfolio_value
+        self.action_hist.at[self.df.index[self.step_count]] = action
+
+        return reward
+
+    @staticmethod
+    def __normalize_action(action):
+        action = np.asarray(action)
+        action = (action + 1.0) / 2.0
+        action_sum = action.sum()
+
+        if np.isclose(action_sum, [0.0, np.nan], equal_nan=True, atol=1e-16).any():
+            warn(f"Received {action_sum}'s as action!")
+            action = np.array([1.0, *np.zeros(action.shape[0] - 1)])
+            action_sum = 1.0
+
+        return action / action_sum
+
+    @staticmethod
+    def __normalize_observation(observation):
+        return (observation - 1.0) * 100.0
 
     @staticmethod
     def __preprocess_df(df, stock_symbols, date_range=None):
--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from environments.dataset import B3_STOCKS, top_ten_stocks
 def __main__():
     np.set_printoptions(linewidth=np.inf)
     env = gym.make('DayTrading-v0', df=B3_STOCKS, stock_symbols=top_ten_stocks['2017'],
-                   date_range=('2015-01-01', '2015-01-06'), W=3)
+                   date_range=('2015-01-01', '2015-01-06'), window_length=3)
 
     print('---------- Initial state ----------')
     state = env.reset()
     env.render()
     print(state, '\n')
 
-    print('\n---------- Next state ----------')
-    state, reward, done, _ = env.step(np.asarray([1.0, .0, .0, .0, .0, .0, .0, .0, .0, .0, .0]))
-    print(f"Reward: {reward}, Done? {done}\n")
+    print('---------- Next state ----------')
+    state, reward, done, info = env.step([-1., 1., -1., -1., -1., -1., -1., -1., -1., -1., -1.])
+    print(f"Reward: {reward}, Done? {done}, Info: {info}\n")
     env.render()
     print(state, '\n')
 
-    print('\n---------- Next state ----------')
-    state, reward, done, _ = env.step(np.asarray([.0, 1.0, .0, .0, .0, .0, .0, .0, .0, .0, .0]))
-    print(f"Reward: {reward}, Done? {done}\n")
+    print('---------- Next state ----------')
+    state, reward, done, info = env.step([-1., 1., -1., -1., -1., -1., -1., -1., -1., -1., -1.])
+    print(f"Reward: {reward}, Done? {done}, Info: {info}\n")
     env.render()
     print(state, '\n')
 
